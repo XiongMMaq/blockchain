a1d2fa2dd8a96ee558390aca79acd32d78911d13
XiongMM <xiongmm920@163.com>
2018-02-23 15:43:05 +0800

    初始化区块链

---
+++ src/main/com/xmm/bk/EncryptUtils.java
@@ -0,0 +1,7 @@
+package com.xmm.bk;
+
+/**
+ * Created by XiongMM on 2018/2/23.
+ */
+public class EncryptUtils {
+}
---
+++ src/main/com/xmm/bk/TestMain.java
@@ -0,0 +1,15 @@
+package com.xmm.bk;
+
+import java.util.List;
+
+/**
+ * Created by XiongMM on 2018/2/23.
+ */
+public class TestMain {
+
+    public List<Block>
+
+    public static void main(String[] args){
+
+    }
+}
